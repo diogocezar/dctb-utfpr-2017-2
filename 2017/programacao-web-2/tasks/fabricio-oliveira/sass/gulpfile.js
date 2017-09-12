130cf343efef8af8d5735a60d19a51d362c4fefb
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2017-09-12 20:29:59 -0300

    Atividade04 - SASS. Fabricio Jhonata Sousa de Oliveira.

---
+++ 2017/programacao-web-2/tasks/fabricio-oliveira/sass/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var gutil = require('gulp-util');
+
+function swallowError (error) {
+    
+    // If you want details of the error in the console
+    console.log(error.toString())
+
+    this.emit('end')
+}
+
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload());
+});
+
+
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .on('error', swallowError)
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('default', ['serve']);
